91bbcbd9b688daa57576fb5d5e510845cf066b45
ERNI\cagi <gilbe.cao@gmail.com>
2020-04-22 14:28:27 +0200

    add semi rule

--- projects/mercat-kzero-lib/src/lib/mercat-kzero-lib.service.spec.ts
+++ projects/mercat-kzero-lib/src/lib/mercat-kzero-lib.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from '@angular/core/testing'
+import { TestBed } from '@angular/core/testing';
 
-import { MercatKzeroLibService } from './mercat-kzero-lib.service'
+import { MercatKzeroLibService } from './mercat-kzero-lib.service';
 
 describe('MercatKzeroLibService', () => {
-  let service: MercatKzeroLibService
+  let service: MercatKzeroLibService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({})
-    service = TestBed.inject(MercatKzeroLibService)
-  })
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(MercatKzeroLibService);
+  });
 
   it('should be created', () => {
-    expect(service).toBeTruthy()
-  })
-})
+    expect(service).toBeTruthy();
+  });
+});
--- projects/mercat-kzero-lib/src/public-api.ts
+++ projects/mercat-kzero-lib/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of mercat-kzero-lib
  */
 
-export * from './lib/mercat-kzero-lib.service'
-export * from './lib/mercat-kzero-lib.component'
-export * from './lib/mercat-kzero-lib.module'
+export * from './lib/mercat-kzero-lib.service';
+export * from './lib/mercat-kzero-lib.component';
+export * from './lib/mercat-kzero-lib.module';
