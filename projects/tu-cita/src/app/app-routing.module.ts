ca1c49c08f17e199c2ad15291b962b3313df4402
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2020-05-08 17:35:06 +0200

    Creating the reservation and reservationDetails component

--- projects/tu-cita/src/app/app-routing.module.ts
+++ projects/tu-cita/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductMapComponent } from 'projects/mercat-kzero-web/src/app/views/map-view/product-map/product-map.component';
+import { ReservationComponent } from './components/reservation/reservation.component';
+import { ReservationDetailsComponent } from './components/reservation-details/reservation-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: ProductMapComponent },
+  { path: 'reservation', component: ReservationComponent },
+  { path: 'reservationDetails', component: ReservationDetailsComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projects/tu-cita/src/app/app.module.ts
+++ projects/tu-cita/src/app/app.module.ts
@@ -6,16 +6,28 @@ import { AppComponent } from './app.component';
 import { MapViewModule } from './map-view/map-view.module';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReservationComponent } from './components/reservation/reservation.component';
+import { ReservationDetailsComponent } from './components/reservation-details/reservation-details.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ReservationComponent, ReservationDetailsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MapViewModule,
     MatFormFieldModule,
     MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatCardModule,
+    MatListModule,
+    MatButtonModule,
     BrowserAnimationsModule
   ],
   bootstrap: [AppComponent]
