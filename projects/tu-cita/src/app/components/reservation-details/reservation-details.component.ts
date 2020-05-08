b5feb6b08e13eeee9a4be4f3357b44b638ab271b
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2020-05-08 17:36:17 +0200

    Resolvin problems

--- projects/tu-cita/src/app/components/reservation-details/reservation-details.component.ts
+++ projects/tu-cita/src/app/components/reservation-details/reservation-details.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-reservation-details',
   templateUrl: './reservation-details.component.html',
   styleUrls: ['./reservation-details.component.scss']
 })
-export class ReservationDetailsComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class ReservationDetailsComponent {}
