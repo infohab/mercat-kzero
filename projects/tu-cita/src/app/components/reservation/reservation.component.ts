133f48a5635c41227f218def980a3cea13169584
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2020-05-09 14:01:36 +0200

    Adding validators and improving styles

--- projects/tu-cita/src/app/components/reservation/reservation.component.ts
+++ projects/tu-cita/src/app/components/reservation/reservation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
@@ -8,7 +8,7 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class ReservationComponent {
   public reservationForm = this.formBuilder.group({
-    name: ['', Validators.required],
+    name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(35)]],
     serviceType: ['', Validators.required],
     employee: ['', Validators.required],
     date: ['', Validators.required],
@@ -16,4 +16,35 @@ export class ReservationComponent {
   });
 
   public constructor(private formBuilder: FormBuilder) {}
+
+  public onServiceChange(): void {
+    this.employee.setValue(null);
+    this.date.setValue(null);
+    this.time.setValue(null);
+  }
+
+  public onEmployeeChange(): void {
+    this.date.setValue(null);
+    this.time.setValue(null);
+  }
+
+  public onDateChange(): void {
+    this.time.setValue(null);
+  }
+
+  public get name() {
+    return this.reservationForm.get('name');
+  }
+
+  public get employee() {
+    return this.reservationForm.get('employee');
+  }
+
+  public get date() {
+    return this.reservationForm.get('date');
+  }
+
+  public get time() {
+    return this.reservationForm.get('time');
+  }
 }
