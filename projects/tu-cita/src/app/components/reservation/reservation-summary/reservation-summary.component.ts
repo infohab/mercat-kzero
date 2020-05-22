fd9adaf4ef6f46350c26eb44b057ddc97bbdcf8b
Gilberto Cao <gilbe.cao@gmail.com>
2020-05-22 09:55:48 +0200

    add calendar link

--- projects/tu-cita/src/app/components/reservation/reservation-summary/reservation-summary.component.ts
+++ projects/tu-cita/src/app/components/reservation/reservation-summary/reservation-summary.component.ts
@@ -7,5 +7,14 @@ import { ReservationService } from '../../../core/reservation/reservation.servic
   styleUrls: ['./reservation-summary.component.scss']
 })
 export class ReservationSummaryComponent {
+  public calendarLink = (): string => {
+    const baseCalendarUrl = 'https://www.google.com/calendar/render?action=TEMPLATE';
+    const title = 'Corte de cabello en Montjuic';
+    const description =
+      'TuCita para corte de cabello en Montjuic. Recuerda seguirnos en todas las redes sociales y ganar descuentos invitando amigos a utilizar nuestra aplicación.';
+    const startDate = new Date(2020, 4, 22, 10, 45).toISOString().replace(/-|:|\.\d\d\d/g, '');
+    const endDate = new Date(2020, 4, 22, 11, 30).toISOString().replace(/-|:|\.\d\d\d/g, '');
+    return `${baseCalendarUrl}&text=${title}&dates=${startDate}/${endDate}&details=${description}&sf=true&output=xml`;
+  };
   constructor(public reservationService: ReservationService) {}
 }
