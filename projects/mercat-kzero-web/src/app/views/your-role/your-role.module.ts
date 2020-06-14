0dcbf6d288664755cda100c08bf22e472d5ffc0e
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2020-06-14 18:12:15 +0200

    Doing header responsive

--- projects/mercat-kzero-web/src/app/views/your-role/your-role.module.ts
+++ projects/mercat-kzero-web/src/app/views/your-role/your-role.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { YourRoleComponent } from './your-role.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [YourRoleComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   exports: [YourRoleComponent]
 })
 export class YourRoleModule {}
